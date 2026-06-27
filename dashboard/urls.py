5fb316f64a34d78eaa1668ee78994777f95d1bf3
BilalQanoa <bilalqanoa@gmail.com>
2026-06-27 11:50:15 +0300

    feat: implement interactive templates selection with live preview and settings view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,4 +5,6 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('home/', views.dashboard_view, name='main_dashboard'),
+    path('templates/', views.templates_view, name='templates_dashboard'),
+    path('settings/', views.settings_view, name='setting_dashboard'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -11,4 +11,14 @@ def dashboard_view(request):
     
     return render(request, 'dashboard/dashboard.html', {'user': user})
 
-    
+def templates_view(request):
+    if 'user_id' not in request.session:
+        return redirect('accounts:login')
+    user = User.objects.get(id=request.session['user_id'])
+    return render(request, 'dashboard/templates_dashboard.html', {'user': user})
+
+def settings_view(request):
+    if 'user_id' not in request.session:
+        return redirect('accounts:login')
+    user = User.objects.get(id=request.session['user_id'])
+    return render(request, 'dashboard/setting_dashboard.html', {'user': user})
